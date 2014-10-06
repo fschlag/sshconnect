5ae7b4a8308c664b0c6a331e45763e4fb17e38d2
Florian Schlag <github@florianschlag.de>
2014-10-06 15:21:57 +0200

    SSH bookmark tool
    SSH bookmark tool you can use form your shell. Easily configurable with
    an json style config file.

---
+++ sshconnect.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+import sys
+import json
+from subprocess import call
+from os.path import expanduser
+#
+#
+# Simple ssh bookmark tool
+# Configuration structure is:
+# 
+# {
+# 	"connections": [
+# 		{
+# 			"name": "Name of first connection",
+# 			"connection": "username@server",
+# 			"key": ""
+# 		},
+# 		{
+# 			"name": "Name of second connection",
+# 			"connection": "username@server2",
+# 			"key": "/Path/to/identity/file"
+# 		}
+# 	]
+# }
+#
+# Copyright 2014 by Florian Schlag
+# Version 0.1
+
+# Path to configuration file
+HOME = expanduser("~")
+CONFIG_FILE = HOME + "/.sshconnect"
+
+
+##################################################
+# Do not change anything below here
+##################################################
+def main():
+	config = openConfig()
+	if config is not None:
+		con = chooseConnection(config)
+		if con is not None:
+			connectTo(con, config['connections'])
+
+def openConfig():
+	try:
+		config = json.loads(open(CONFIG_FILE).read())
+		return config
+	except IOError:
+		print "Config file: \"" + CONFIG_FILE + "\" not found"
+	return None
+
+def chooseConnection(config):
+	while True:
+		numConnections = printMenu(config['connections'])
+		try:
+			print "Choose connection:",
+			conn = int(raw_input())	# Read connection number
+			if conn > numConnections or conn < 1:
+				print "Invalid connection number. Please try again!"
+			else:
+				return conn
+		except ValueError:
+			print "Only digits are allowed! Please try again!"
+		except KeyboardInterrupt:
+			break
+	return None
+
+def printMenu(connections):
+	num = 0
+	print "Available connections:"
+	for conn in connections:
+		num += 1
+		print "   " + str(num) + "\t" + conn['name']
+	return num
+
+def connectTo(conNumber, connections):
+	num = 0
+	for conn in connections: # Find the selected connection
+		num += 1
+		if num == conNumber:
+			if len(conn['key']) > 1: # Connect with ssh key
+				call(["ssh", "-i" + conn['key'], conn['connection']])
+			else: # Connect without ssh key
+				call(["ssh", conn['connection']])
+
+if __name__ == "__main__":
+    sys.exit(main())
